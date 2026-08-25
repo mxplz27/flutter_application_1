e49daae29769ae91aaaa740e66f2499abff9c140
mxplz27 <palencialopez@gmail.com>
2026-08-25 07:10:47 -0500

    Fix build web release con base-href

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"69c8c61792f04cc809dfef0c910414fb9afc06
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2019133674" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1727138548" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
